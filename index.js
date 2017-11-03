29b5cf928de161e755e24018728893f064c0d147
Helen Liutongco <bellpickle@gmail.com>
2017-11-03 20:34:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  debugger;
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+//You want to return lis[3] --> this passes the test
+return test;
+}
